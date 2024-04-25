75dfcbcadd6acf14982922281f397716c1f491ac
Josede2450 <82405137+Josede2450@users.noreply.github.com>
2024-04-25 10:24:04 -0400

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"; // For use Mongo in this page
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema); // Importing to moongose
+
+export default User; // For using in the application
